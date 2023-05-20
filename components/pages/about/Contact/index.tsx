cc7b1edcbb9f5585548d4cc4b4b2a7755fbcbc85
gillyhuga <gilly.huga@yahoo.co.id>
2023-05-20 10:23:13 +0700

    update: text color

--- components/pages/about/Contact/index.tsx
+++ components/pages/about/Contact/index.tsx
@@ -7,10 +7,10 @@ const Contact = () => {
                 <h1 className="text-3xl py-5">
                     <RoughNotation type="underline" show strokeWidth={2} padding={2} color="#999999">Contact</RoughNotation>
                 </h1>
-                <p>
+                <p className="text-secondary dark:prose-dark">
                     Want to talk about technology related project or just drink something tasty together?
                 </p>
-                <p className="pt-5">
+                <p className="pt-5 text-secondary dark:prose-dark">
                     If yes, please don&apos;t be shy and just reach me at <a className='underline hover:no-underline font-semibold' href='mailto:hi@gillyhuga.com'>hi@gillyhuga.com</a>. Anyway, thanks for visiting my profile :)
                 </p>
             </div>

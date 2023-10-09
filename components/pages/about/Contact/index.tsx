90cfedf42791c40d23d4fa749cee38b496490f90
gillyhuga <gilly.huga@yahoo.co.id>
2023-10-09 21:49:41 +0700

    feat: add animation using framer-motion

--- components/pages/about/Contact/index.tsx
+++ components/pages/about/Contact/index.tsx
@@ -1,3 +1,4 @@
+import  AnimationLayout  from "@/components/layout/AnimationLayout";
 import { RoughNotation } from "react-rough-notation";
 
 const Contact = () => {
@@ -7,13 +8,16 @@ const Contact = () => {
                 <h1 className="text-3xl py-5">
                     <RoughNotation type="underline" show strokeWidth={2} padding={2} color="#999999">Contact</RoughNotation>
                 </h1>
-                <p className="text-secondary dark:prose-dark">
-                    Want to talk about technology related project or just drink something tasty together?
-                </p>
-                <p className="pt-5 text-secondary dark:prose-dark">
-                    If yes, please don&apos;t be shy and just reach me at <a className='underline hover:no-underline font-semibold' href='mailto:hello@gillyhuga.com'>hello@gillyhuga.com</a>. Anyway, thanks for visiting my profile :)
-                </p>
+                <AnimationLayout>
+                    <p className="text-secondary dark:prose-dark">
+                        Want to talk about technology related project or just drink something tasty together?
+                    </p>
+                    <p className="pt-5 text-secondary dark:prose-dark">
+                        If yes, please don&apos;t be shy and just reach me at <a className='underline hover:no-underline font-semibold' href='mailto:hello@gillyhuga.com'>hello@gillyhuga.com</a>. Anyway, thanks for visiting my profile :)
+                    </p>
+                </AnimationLayout>
             </div>
+
         </>
     )
 }
